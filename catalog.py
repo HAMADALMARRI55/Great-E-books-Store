1b9cbe7cfe98deb8ba444fe6584488de27fac666
HAMADALMARRI55 <hamadmarmeri@gmail.com>
2024-11-07 15:27:22 +0530

    Add files via upload

--- catalog.py
+++ catalog.py
@@ -19,3 +19,23 @@ class Catalog:
         """
         if isinstance(ebook, EBook):
             self.__ebooks.append(ebook)
+
+    def remove_ebook(self, ebook):
+        """
+        Removes an EBook from the catalog.
+        """
+        if ebook in self.__ebooks:
+            self.__ebooks.remove(ebook)
+
+    @property
+    def ebooks(self):
+        """
+        Returns the list of e-books in the catalog.
+        """
+        return self.__ebooks
+
+    def __str__(self):
+        """
+        Returns a string representation of the Catalog.
+        """
+        return f"Catalog with {len(self.__ebooks)} e-books."
